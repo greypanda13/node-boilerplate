2c024be66dbd38747cb64dee173c3bfd6a542317
greypanda13 <greypanda13@gmail.com>
2019-04-11 11:53:00 -0700

    home page stub up and running, EJS home initialized

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 // require necessary modules
 let express = require("express");
+let layouts = require("express-ejs-layouts");
 
 // declare express app
 let app = express();
@@ -11,14 +12,14 @@ let app = express();
 app.set("view engine", "ejs");
 
 // include (use) middleware
-
+app.use(layouts);
 
 // include routes from controllers
 
 
 // make a home route: GET /
 app.get("/", (req, res) => {
-  res.send("<h1>STUB</h1>");
+  res.render("home");
 });
 
 // listen on specified port
