d46a15f1ce9fa1d3abc6ae0b24254a67ef452f7e
greypanda13 <greypanda13@gmail.com>
2019-04-11 11:47:52 -0700

    home page stubbed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node/Express/PostgreSQL Boilerplate
+
+in development
+
+
+
+## routes |
+TBD - table of available routes
+
+## User model setup
+
+TBD - diagram of user |
+## usage
+
+TBD - directions on how to use this boilerplate code in future projects
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // include .env variables
+require("dotenv").config();
 
 // require necessary modules
+let express = require("express");
 
 // declare express app
+let app = express();
 
 // set view engine
+app.set("view engine", "ejs");
 
 // include (use) middleware
 
+
 // include routes from controllers
 
+
 // make a home route: GET /
+app.get("/", (req, res) => {
+  res.send("<h1>STUB</h1>");
+});
 
 // listen on specified port
+app.listen(process.env.PORT || 3000);
