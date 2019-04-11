28aea80d474290dc2ff8e126361accaa7a88a48a
greypanda13 <greypanda13@gmail.com>
2019-04-11 11:37:45 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// include .env variables
+
+// require necessary modules
+
+// declare express app
+
+// set view engine
+
+// include (use) middleware
+
+// include routes from controllers
+
+// make a home route: GET /
+
+// listen on specified port
